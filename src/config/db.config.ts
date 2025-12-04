a6e0f5505cd35683f659432a83b6f427f638146a
TOMJID <tomjidhuda04@gmail.com>
2025-12-04 21:27:45 +0600

    feat: Define `users` table schema and add error handling to database initialization.

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -8,17 +8,24 @@ const pool= new Pool({
 
 export const initDB= async()=>{
   //? User Table
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    
-    
-    
-    
-    
-    
+
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      phone VARCHAR(20) ,
+      role VARCHAR(50),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW() 
+     )`
     )
 
-    
-    `)
+     console.log("Database initialized successfully");
+  } catch (error: any) {
+      console.error("Error initializing database:", error);
+  }
 
 }
