d4fb40b1ec6139625207ac19f9dc392800c5447c
TOMJID <tomjidhuda04@gmail.com>
2025-12-05 15:25:24 +0600

    feat: Enhance auth middleware to handle token verification and add user type to request

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,35 +1,43 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { ENV } from "../config/dotenv.config";
 
 const auth = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     const authToken = req.headers.authorization;
 
-    //? check if auth token is present
-    if (!authToken) {
-      return res.status(401).json({ message: "You are not Authorized" });
+    try {
+      //? check if auth token is present
+      if (!authToken) {
+        return res.status(401).json({ message: "You are not Authorized" });
+      }
+
+      //? extract token from Bearer <token>
+      const tokens = authToken.split(" ");
+
+      //? check if token format is valid
+      if (tokens.length !== 2 || tokens[0] !== "Bearer") {
+        return res
+          .status(401)
+          .json({ message: "Invalid Authorization header format" });
+      }
+
+      //? get the token part
+      const token = tokens[1]!;
+
+      //? verify auth token
+      const secretKey = ENV.jwtSecret;
+      const decodedToken = jwt.verify(token, secretKey);
+
+      req.user = decodedToken as JwtPayload;
+
+      next();
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
-
-    //? extract token from Bearer <token>
-    const tokens = authToken.split(" ");
-
-    //? check if token format is valid
-    if (tokens.length !== 2 || tokens[0] !== "Bearer") {
-      return res
-        .status(401)
-        .json({ message: "Invalid Authorization header format" });
-    }
-
-    //? get the token part
-    const token = tokens[1]!;
-
-
-    //? verify auth token
-    const secretKey = ENV.jwtSecret;
-    const decodedToken = jwt.verify(token, secretKey);
-    console.log(decodedToken);
-    next();
   };
 };
 
