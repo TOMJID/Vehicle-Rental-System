bd802704f46c22243f257e0b3e1c0d9f25ea837c
TOMJID <tomjidhuda04@gmail.com>
2025-12-07 16:46:46 +0600

    feat(app): add root route with welcome message and 404 handler and organize routes
    - Implemented GET / route as an API entry point
    - Added personalized welcome message
    - Added global 404 "Route not found" handler

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,14 @@ app.use(express.json());
 //? initialize database
 initDB();
 
+//? root route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the Vehicle Rental System API where you can rent your favorite vehicle by Tomjid",
+  });
+});
+
 //? auth route
 app.use("/api/v1/auth", authRouter);
 
@@ -24,4 +32,15 @@ app.use("/api/v1/vehicles", vehicleRouter);
 
 //? booking route
 app.use("/api/v1/bookings", bookingRoute);
+
+//! 404 route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
+
 export default app;
